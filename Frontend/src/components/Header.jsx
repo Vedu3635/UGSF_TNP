4995d1d8317554aac67733254beb245bf3533df7
Your Name <you@example.com>
2024-09-10 14:52:33 +0530

    add navbar 2nd time

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
                 }
             </div>
             {/* linke items */}
-            <ul className={md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}}>
+            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
                 {
                     Links.map((link) => (
                     <li className='md:ml-8 md:my-0 my-7 font-semibold'>
