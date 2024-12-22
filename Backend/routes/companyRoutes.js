88958a933d85f9d40c89cfb194a9095c43ad9e2a
Kalpmodha <kalpmodha@gmail.com>
2024-12-22 14:13:43 +0530

    set authMiddleware to apis

--- Backend/routes/companyRoutes.js
+++ Backend/routes/companyRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const companyController = require("../controllers/companyController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // Route to add company details
-router.post("/companies", companyController.addCompany);
+router.post("/companies", authMiddleware, companyController.addCompany);
 
 module.exports = router;
