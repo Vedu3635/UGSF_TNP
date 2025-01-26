ddcb5c01cb6a15e447b37cb420bd0953807b309e
Kalpmodha <kalpmodha@gmail.com>
2025-01-26 20:20:38 +0530

    added authMiddleware to delete student api.

--- Backend/routes/deleteStudent.js
+++ Backend/routes/deleteStudent.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { deleteStudent } = require('../controllers/deleteStudent');
+const authMiddleware = require("../middleware/authMiddleware");
 
 // Route for deleting a student
-router.delete('/deleteStudent/:studentId', deleteStudent);
+router.delete('/deleteStudent/:studentId', authMiddleware, deleteStudent);
 
 module.exports = router;
